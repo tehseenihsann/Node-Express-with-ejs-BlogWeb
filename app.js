6db1390257e9627a6a6d78edfcbeed0b1ca6561a
Tehseen Islahi <tehseenihsan33@gmail.com>
2024-02-25 19:31:10 +0500

    add public assets and modules, also connect with mongodb

--- app.js
+++ app.js
@@ -1,13 +1,80 @@
 const express = require("express");
-
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+const Blog = require("./models/blog");
+const { result } = require("lodash");
 // express app
 const app = express();
+
+const dbURI = "mongodb+srv://tehseen:test1234@test-blog.kkto4sq.mongodb.net/";
+mongoose
+  .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) => app.listen(3000))
+  .catch((err) => console.log(err));
 
 // resister view engine
 app.set("view engine", "ejs");
 
 // listen for request
-app.listen(3000);
+
+// -----------------Middleweres---------------------
+// app.use((req, res, next) => {
+//   console.log("new requres made:");
+//   console.log("host", req.hostname);
+//   console.log("path", req.path);
+//   console.log("method", req.method);
+//   next();
+// });
+
+//
+//   app.use((req, res, next) => {
+//     console.log("in the next middlewere");
+//     next();
+//   });
+
+// ---------------------------------------------
+
+// -------Third party middlewere & static files--------------------
+// for cookies, session, etcz
+
+app.use(express.static("public"));
+app.use(morgan("dev"));
+
+// mongoose and mongo sandboz routes
+app.get("/add-blog", (req, res) => {
+  const blog = new Blog({
+    title: "new blog 2",
+    snippet: "about my new blog",
+    body: "more about my new blog",
+  });
+
+  blog
+    .save()
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.get("/all-blogs", (req, res) => {
+  Blog.find()
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.get("/single-blog", (req, res) => {
+  Blog.findById("65db4bbbe476cf6f3a7848eb")
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => console.log(err));
+});
 
 app.get("/", (req, res) => {
   const blogs = [
